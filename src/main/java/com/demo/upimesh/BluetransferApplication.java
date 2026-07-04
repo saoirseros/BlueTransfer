adbfceecdc6326ad552c57b9974abb269ee7754b
Shruti <shruti2904h@gmail.com>
2026-07-04 23:03:53 +0530

    initial springboot setup

---
+++ src/main/java/com/demo/upimesh/BluetransferApplication.java
@@ -0,0 +1,13 @@
+package com.demo.upimesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluetransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluetransferApplication.class, args);
+	}
+
+}
