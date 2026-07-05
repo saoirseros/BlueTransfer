0fb3131e2a65bfad6d586c255608e6d75a5a7d3a
Shruti <shruti2904h@gmail.com>
2026-07-05 21:10:46 +0530

    fields in account

--- src/main/java/com/demo/upimesh/model/Account.java
+++ src/main/java/com/demo/upimesh/model/Account.java
@@ -1,4 +1,24 @@
 package com.demo.upimesh.model;
 
+import jakarta.persistence.*;
+import jakarta.math.BigDecimal;
+
+@Entity
+@Table(name="accounts")
+
 public class Account {
+    @Id
+    private String vpa;
+
+    @column(nullable=false)
+    private String holderName;
+
+    @Column(nullable=false, precision=19, scale==2)
+    private BigDecimal balance;
+
+    @Version
+    private Long version;
+
+    public Account() {} //empty constructor
+
 }
