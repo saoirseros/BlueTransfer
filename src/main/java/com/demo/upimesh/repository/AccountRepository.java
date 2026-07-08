52285e18cdb2db00c4eab8f69a2bc7b5a6b23d7a
Shruti <shruti2904h@gmail.com>
2026-07-08 21:58:24 +0530

    updated AccountRepository

--- src/main/java/com/demo/upimesh/repository/AccountRepository.java
+++ src/main/java/com/demo/upimesh/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.demo.upimesh.repository;
 
+import com.demo.upimesh.model.Account;
 import org.Springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
