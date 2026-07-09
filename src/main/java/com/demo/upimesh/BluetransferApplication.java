ef0289313b0ca04a0fe118687fbc4f857d9a4f23
Shruti <shruti2904h@gmail.com>
2026-07-09 22:48:28 +0530

    Complete ServerKeyHolder

--- src/main/java/com/demo/upimesh/BluetransferApplication.java
+++ src/main/java/com/demo/upimesh/BluetransferApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BluetransferApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BluetransferApplication.class, args);
 	}
-
 }
